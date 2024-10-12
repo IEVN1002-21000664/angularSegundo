09675fdfffba89f421afafca60ae9ce65507ea88
Jose Ceballos <jose.ceballos@na.denso.com>
2024-10-11 20:56:17 -0600

    Primer commit

---
+++ src/app/formulario/ejemplo1/ejemplo1.component.ts
@@ -0,0 +1,38 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+
+interface Usuarios{
+  nombre: string;
+  edad: number;
+  email: string;
+}
+@Component({
+  selector: 'app-ejemplo1',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './ejemplo1.component.html',
+  styles: ``
+})
+export class Ejemplo1Component implements OnInit{
+  formGroup!: FormGroup;
+   
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(): void {
+    this.formGroup = this.initForm();
+    
+  }
+  initForm():FormGroup{
+    return this.fb.group({
+      nombre:[''],
+      edad: [''],
+      email: [''],
+    })
+  }
+
+  onSubmit():void{
+    console.log(this.formGroup.value)
+  }
+}
